f90c6d0adac506484a15010253389db7a6156feb
Freeman <ilambda@hotmail.fr>
2016-05-11 01:02:11 +0200

    ps2sim_setdata|setclock are now static

--- ps2simulator.h
+++ ps2simulator.h
@@ -1,4 +1,6 @@
+#ifndef NULL
 #define NULL 0
+#endif
 #define CLOCK_LENGTH_2 25
 
 static unsigned char* data_pin = NULL;
@@ -10,5 +12,5 @@ static unsigned char i = 0;
 void ps2sim_init(unsigned char* datapin, unsigned char databit, unsigned char* clockpin, unsigned char clockbit);
 void ps2sim_send(unsigned char buffer);
 unsigned char ps2sim_oddparity(unsigned char buffer);
-void ps2sim_setdata(unsigned char value);
-void ps2sim_setclock(unsigned char value);
+static void ps2sim_setdata(unsigned char value);
+static void ps2sim_setclock(unsigned char value);
